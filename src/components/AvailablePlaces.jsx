606713bf6f25c974813a4348555cb16acf67add9
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2025-06-08 20:31:47 +0200

    ♻️ Update : add fetch API function in one way

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,10 +1,23 @@
-import Places from './Places.jsx';
+import { useEffect, useState } from "react";
+import Places from "./Places.jsx";
 
 export default function AvailablePlaces({ onSelectPlace }) {
+  const [availablePlaces, setAvailablePlaces] = useState([]); // This would typically be replaced with a state or prop that holds the available places
+  useEffect(() => {
+    fetch("http://localhost:3000/places") // Adjust the URL to your API endpoint
+      .then((response) => response.json())
+      .then((data) => {
+        setAvailablePlaces(data.places);
+      })
+      .catch((error) => {
+        console.error("Error fetching available places:", error);
+      });
+  }, []); // Empty dependency array means this effect runs once after the initial render
+
   return (
     <Places
       title="Available Places"
-      places={[]}
+      places={availablePlaces}
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
