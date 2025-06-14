2c4fc65055983104ee85d97714991b1634990378
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2025-06-15 00:01:24 +0200

    ♻️ Update : the logic and fetch places by using useFetch hook

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,40 +1,29 @@
-import { useEffect, useState } from "react";
 import Places from "./Places.jsx";
 import ErrorPage from "./ErrorPage.jsx"; // Assuming you have an ErrorPage component for displaying errors
 import { sortPlacesByDistance } from "../loc.js"; // Adjust the import path as necessary
 import { fetchAvailablePlaces } from "../http.js";
+import { useFetch } from "../hooks/useFetch.js"; // Assuming you have a custom hook for fetching data
+
+async function fetchSortedPlaces() {
+  const places = await fetchAvailablePlaces();
+  return new Promise((resolve) => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      const sortedPlaces = sortPlacesByDistance(
+        places,
+        position.coords.latitude,
+        position.coords.longitude
+      );
+      resolve(sortedPlaces);
+    });
+  });
+}
 
 export default function AvailablePlaces({ onSelectPlace }) {
-  const [isLoading, setIsLoading] = useState(false);
-  const [availablePlaces, setAvailablePlaces] = useState([]); // This would typically be replaced with a state or prop that holds the available places
-  const [error, setError] = useState(null);
-  useEffect(() => {
-    async function fetchPlaces() {
-      setIsLoading(true);
-      try {
-        const places = await fetchAvailablePlaces();
-        navigator.geolocation.getCurrentPosition((position) => {
-          const sortedPlaces = sortPlacesByDistance(
-            places,
-            position.coords.latitude,
-            position.coords.longitude
-          );
-          setAvailablePlaces(sortedPlaces);
-          setIsLoading(false);
-        });
-
-        setAvailablePlaces(places); // Ensure we set an empty array if no places are returned
-      } catch (error) {
-        setError({
-          message:
-            error.message || "Failed to fetch places, Pleas try Again later",
-        });
-        console.error("Error fetching available places:", error);
-        setIsLoading(false);
-      }
-    }
-    fetchPlaces();
-  }, []); // Empty dependency array means this effect runs once after the initial render
+  const {
+    isLoading,
+    fetchedData: availablePlaces,
+    error,
+  } = useFetch(fetchSortedPlaces, []);
 
   if (error) {
     return <ErrorPage title="An Error Occured..!" message={error.message} />;
